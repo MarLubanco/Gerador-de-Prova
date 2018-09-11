8e38624c90e5a61db86e22b25852807ce24c3a22
MarLubanco <lubanco@edu.unifil.br>
2018-09-11 00:08:20 -0300

    [PROVA][SERVICE] Documentação dos métodos da service

--- src/main/java/com/br/unifil/gerador/de/prova/service/OpcaoService.java
+++ src/main/java/com/br/unifil/gerador/de/prova/service/OpcaoService.java
@@ -20,26 +20,53 @@ public class OpcaoService {
     @Autowired
     OpcaoRepository opcaoRepository;
 
+
+    /**
+     * Recupera todas as opções cadastradas
+     * @return
+     */
     @GetMapping
     public List<Opcao> getAllOpcoes() {
         return opcaoRepository.findAll();
     }
 
+
+    /**
+     * Recupera uma opções a partir do id da opção
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public Optional<Opcao> getOpcao(@PathVariable("id") Long id) {
         return opcaoRepository.findById(id);
     }
 
+    /**
+     * Salvar uma opção
+     * @param opcao
+     * @return
+     */
     @PostMapping
     public Opcao saveOpcao(@RequestBody Opcao opcao) {
         return opcaoRepository.save(opcao);
     }
 
+
+    /**
+     * Atualizar um opção
+     * @param id
+     * @param opcao
+     * @return
+     */
     @PutMapping("/{id}")
     public Opcao atualizaOpcao(@PathVariable("id") Long id, @RequestBody Opcao opcao) {
         return opcaoRepository.save(opcao);
     }
 
+    /**
+     * Deletar uma opção
+     * @param id
+     */
     @DeleteMapping("/{id}")
     public void deleteOpcao(@PathVariable("id") Long id) {
         opcaoRepository.deleteById(id);
